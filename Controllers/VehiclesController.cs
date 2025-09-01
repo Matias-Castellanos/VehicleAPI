4bcc5efc4ccce202903700cc67f989a4b3ab79aa
Matias Castellanos <a2024112625@estudiantes.upsa.edu.bo>
2025-09-01 19:40:21 -0400

    Commit 4 - Controller para Vehicle

---
+++ Controllers/VehiclesController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace VehicleAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class VehiclesController : ControllerBase
+{
+
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+        return new string[] { "value1", "value2" };
+    }
+
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+        return "value";
+    }
+
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+        
+    }
+
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+        
+    }
+}
